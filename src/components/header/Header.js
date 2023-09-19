ef534e45bb8c4b4a8754f034ba2be165c347fa50
Jay Termulo <jetermulo@gmail.com>
2023-09-19 15:17:00 +0800

    styles:convert tailwind css style to css module tailwind

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,14 @@
 import { Link } from 'react-scroll'
 import logoSource from '../../assets/bulakenya-logo.png'
 import styles from './Header.module.scss'
-import { useState } from 'react'
-
-import { Modal } from 'react-daisyui'
-
 
 const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const logo = (
-        <div className="flex">
-            <a className="btn btn-ghost normal-case text-xl text-red-500">
-            <img src={logoSource} alt="bulakenya-logo" width={45}/>
-                <h1 className='text-lg md:text-lg lg:text-2xl m-0'>Bulakenya Gift Shop</h1>
+        <div className={`flex ${styles.logo}`}>
+            <a className="btn btn-ghost">
+                <img src={logoSource} alt="bulakenya-logo" width={45}/>
+                <h1>Bulakenya Gift Shop</h1>
             </a>
         </div>
     )
@@ -21,13 +16,13 @@ const Header = () => {
   return (
     <header>
         <div className=""></div>
-        <div className="drawer drawer-end container m-0 p-0">
-            <input id="my-drawer-4" type="checkbox" className="drawer-toggle m-0 p-0" /> 
+        <div className="drawer drawer-end container">
+            <input id="my-drawer-4" type="checkbox" className="drawer-toggle" /> 
             {/* Header */}
-            <div className="drawer-content flex flex-col m-0 p-0">
-                <div className="w-full navbar bg-white flex flex-row justify-between m-0 p-0">
+            <div className={`drawer-content ${styles['drawer-content']}`}>
+                <div className={`navbar ${styles.navbar}`}>
                     {logo} 
-                    <div className="hidden md:flex">
+                    <div className={styles['menu-box']}>
                         <ul className="menu menu-horizontal">
                         {/* Navbar menu content here */}
                             <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
@@ -38,7 +33,7 @@ const Header = () => {
                     </div>
 
                     {/* Button */}
-                    <button className="btn btn-sm md:btn-lg rounded-none bg-red-500 text-slate-100 hover:btn-neutral hidden md:flex">Sign Up</button>
+                    <button className={`btn btn-sm ${styles['sign-up']}`}>Sign Up</button>
 
 
                            {/* Menu Icon */}
@@ -50,15 +45,15 @@ const Header = () => {
                 </div>
             </div> 
             {/* mobile navbar */}
-            <div className="drawer-side z-10">
+            <div className={`drawer-side ${styles['drawer-side']}`}>
                 <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content rounded-bl-full rounded-tl-2xl ">
+                <ul className="menu">
                     {logo}
-                    <li className='mt-4'><Link to="home" smooth={true} duration={500}>Home</Link></li>
+                    <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
                     <li><Link to="products" smooth={true} duration={500}>Products</Link></li>
                     <li><Link to="about" smooth={true} duration={500}>About</Link></li>
                     <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
-                    <button className="btn btn-sm md:btn-lg rounded-none bg-red-500 text-slate-100 hover:btn-neutral rounded-md mt-5">Sign Up</button>
+                    <button className={`btn btn-sm`}>Sign Up</button>
                 </ul>
             </div>
         </div>
