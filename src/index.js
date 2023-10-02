341e3a41494300f82bdc08d2c3ca64cfdc1ebf35
Jay Termulo <jetermulo@gmail.com>
2023-10-02 11:04:04 +0800

    bugfix: Context API provider error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import ModalContext, { ModalContextProvider } from './context/ModalContext';
+import { ModalContextProvider } from './context/ModalContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
