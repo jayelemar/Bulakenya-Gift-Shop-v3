103f8f0c83f43c495fb27f92f55e91c5ae42d3b4
Jay Termulo <jetermulo@gmail.com>
2023-10-03 08:11:06 +0800

    build: auth pages

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import styles from './auth.module.scss'
+import registerImg from '../../assets/register.svg'
+import Card from '../../components/card/Card'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-
-    <section>
-        <div style={{ height: '60px' }} className='bg-red-400 container'></div>
-        <div className="container">
-            <h4>Register.js</h4>
+<section>
+        <div style={{ height: '60px' }} className='bg-slate-100'></div>
+        <div className={`container slide-up ${styles.auth} flex-row-reverse`}>
+            <div className={styles.img}>
+                <img src={registerImg} alt="register-img" width={320}/>
+            </div>
+            <Card>
+                <div className={styles.form}>
+                    <h3>Register</h3>
+                    <form>
+                        <input type="text" placeholder='Email' required />
+                        <input type="password" placeholder='Password' required />
+                        <input type="password" placeholder='Confirm Password' required />
+                        <button>Register</button>
+                    </form>
+                    <span className={styles.register}>
+                        <p>Already have an account?</p>
+                        <Link to="/login"> &nbsp; Login</Link>
+                    </span>
+                </div>
+            </Card>
+        
         </div>
-    </section>    
+    </section>   
 
   )
 }
--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,15 +1,34 @@
 import React from 'react'
+import styles from './auth.module.scss'
+import Card from '../../components/card/Card'
+import resetImg from '../../assets/forget.svg'
+import { Link } from 'react-router-dom'
 
 const Reset = () => {
   return (
-
     <section>
-        <div style={{ height: '60px' }} className='bg-red-400 container'></div>
-        <div className="container">
-        <h4>Reset.js</h4>
+        <div style={{ height: '60px' }} className='bg-slate-100'></div>
+        <div className={`container slide-up ${styles.auth}`}>
+            <div className={styles.img}>
+                <img src={resetImg} alt="reset-img" width={300}/>
+            </div>
+                <Card>
+                    <div className={styles.form}>
+                        <h3>Reset Password</h3>
+                        <form>
+                            <input type="text" placeholder='Email' required />
+                            <button>Reset Password</button>
+                            <div className={styles.links}>
+                                <Link to="/login">-Login</Link>
+                                <Link to="/register">-Register</Link>
+                            </div>
+                        </form>
+
+                    </div>
+                </Card>
+        
         </div>
     </section>
-
   )
 }
 
