e57cb5b88857fb703fc2696d7278f255a5202877
Jay Termulo <jetermulo@gmail.com>
2023-10-11 07:28:47 +0800

    styles: edit location of display name

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,6 +11,7 @@ import { FaUserCircle } from 'react-icons/fa'
 import { useDispatch } from 'react-redux'
 import { SET_ACTIVE_USER, REMOVE_ACTIVE_USER } from '../../redux/features/auth/authSlice';
 import ShowOnLogin from '../hiddenLink/ShowOnLogin';
+import ShowOnLogout from '../hiddenLink/ShowOnLogout';
 
 
 
@@ -81,63 +82,76 @@ const Header = () => {
                     <div className={`navbar ${styles.navbar}`}>
                         {logo}
                         <div className={styles['menu-box']}>
-                                {/* Horizontal Navbar menu */}
-                                {location.pathname === '/' ? (
-                                    <ul className="menu menu-horizontal">
-                                    <li>
-                                        <ScrollLink to="home" smooth={true} duration={500}>
-                                            Home
-                                        </ScrollLink>
-                                    </li>
-                                    <li>
-                                        <ScrollLink to="products" smooth={true} duration={500}>
-                                            Products
-                                        </ScrollLink>
-                                    </li>
-                                    <li>
-                                        <ScrollLink to="about" smooth={true} duration={500}>
-                                            About
-                                        </ScrollLink>
-                                    </li>
-                                    <li>
-                                        <ScrollLink to='/#' onClick={ openModal }>
-                                            Contact
-                                        </ScrollLink>
-                                    </li>
-                                    </ul>
-                                ) : (
-                                    <div></div>
-                                )}
+                            {/* Horizontal Navbar menu */}
+                            {location.pathname === '/' ? (
+                                <ul className="menu menu-horizontal">
+                                <li>
+                                    <ScrollLink to="home" smooth={true} duration={500}>
+                                        Home
+                                    </ScrollLink>
+                                </li>
+                                <li>
+                                    <ScrollLink to="products" smooth={true} duration={500}>
+                                        Products
+                                    </ScrollLink>
+                                </li>
+                                <li>
+                                    <ScrollLink to="about" smooth={true} duration={500}>
+                                        About
+                                    </ScrollLink>
+                                </li>
+                                <li>
+                                    <ScrollLink to='/#' onClick={ openModal }>
+                                        Contact
+                                    </ScrollLink>
+                                </li>
+                                </ul>
+                            ) : (
+                                <div></div>
+                            )}
                         </div>
+                        <ShowOnLogin>
+                        <div className="dropdown dropdown-bottom dropdown-end">
+                            <label tabIndex={0} className="btn m-1 hidden lg:flex">Hi, {displayName}</label>
+                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <ShowOnLogin>
+                                    <li><RouterLink to="/" onClick={logOutUser} className='justify-center'>Logout</RouterLink></li>
+                                </ShowOnLogin>
+                            </ul>
+                        </div>
+                        </ShowOnLogin>
                         
-                        {/* Button */}
-                        <ShowOnLogin>
-                            <RouterLink><FaUserCircle size={16}/> &nbsp; Hi, {displayName} </RouterLink>
-                        </ShowOnLogin>
 
-                        <ShowOnLogin>
-                            <RouterLink to="/" onClick={logOutUser}>Logout</RouterLink>
-                        </ShowOnLogin>
 
-                        {location.pathname === '/' ? (
-                            <RouterLink to='/login'>
-                                <button className={`btn btn-sm ${styles['sign-up']}`}>Sign In</button>
-                            </RouterLink>
-                        ) : (
-                            <RouterLink to='/#'>
-                                <button className={`btn btn-sm ${styles['sign-up']}`}>Back to Home</button>
-                            </RouterLink>
-                        )}
+
 
                         
+                        
+                        <ShowOnLogout>
+                            {location.pathname === '/' ? (
+                                <RouterLink to='/login'>
+                                    <button className={`btn btn-sm ${styles['sign-up']}`}>Sign In</button>
+                                </RouterLink>
+                            ) : (
+                                <RouterLink to='/#'>
+                                    <button className={`btn btn-sm ${styles['sign-up']}`}>Back to Home</button>
+                                </RouterLink>
+                            )}
+                        </ShowOnLogout>
+
+                        <ShowOnLogin><label tabIndex={0} className="m-1 lflex hidden sm:flex lg:hidden relative left-6"><FaUserCircle size={16}/> &nbsp; Hi, {displayName}</label></ShowOnLogin>
+
                         {/* Menu Icon */}
-                        <div className="flex md:hidden">
+                        <div className="flex lg:hidden">
                             <label htmlFor="my-drawer-4" className="btn btn-square btn-ghost">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                             </label>
+                            
                         </div>
+                        
                     </div>
                 </div>
+                
                 {/* mobile navbar */}
                 <div className={`drawer-side ${styles['drawer-side']}`}>
                     <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
@@ -145,6 +159,7 @@ const Header = () => {
                     {location.pathname === '/' ? (
                         <ul className="menu">
                             {logo}
+                            
                             <li>
                                 <ScrollLink
                                     to="home"
@@ -184,9 +199,19 @@ const Header = () => {
                                 Contact
                             </ScrollLink>
                             </li>
-                            <RouterLink to='/login'>
-                                <button className={`btn btn-sm w-full ${styles['sign-up']}`}>Sign In</button>
-                            </RouterLink>
+                            <li>
+                            <ShowOnLogin>
+                                <RouterLink to="/" onClick={logOutUser} className='justify-center border border-solid border-slate-400 item-end'>Logout</RouterLink>
+                            </ShowOnLogin>
+                            </li>
+                            <ShowOnLogout>
+                                
+                                    <button className={`btn btn-sm w-full ${styles['sign-up']}`}><RouterLink to='/login'>Sign In</RouterLink></button>
+                                
+                            </ShowOnLogout>
+
+                            
+                            
                         </ul>
 
                     ) : (
