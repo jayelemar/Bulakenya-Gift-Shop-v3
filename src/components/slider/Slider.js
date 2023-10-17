88c23fb6ef915d6bbaaf4c1dc5b9161da8532154
Jay Termulo <jetermulo@gmail.com>
2023-10-18 06:36:24 +0800

    bugfix: console error

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,85 +1,74 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
 import { RxDotFilled } from 'react-icons/rx'
 import styles from './Slider.module.scss'
 import { sliderData } from './slider-data';
-import { useEffect } from 'react';
 
 const Slider = () => {
-    
     const [currentSlide, setCurrentSlide] = useState(0)
-    const slideLength = sliderData.length;
-    // console.log(slideLength);
-
-    const autoScroll = true;
+    const [autoScroll, setAutoScroll] = useState(true)
+    const intervalTime = 5000;
     let slideInterval;
-    let intervalTime = 5000;
-
+    const slideLength = sliderData.length
+    
     const handleNextSlide = () => {
         setCurrentSlide(currentSlide === slideLength -1 ? 0 : currentSlide + 1)
     };
+
     const handlePrevSlide = () => {
-        setCurrentSlide(currentSlide === 0 ? slideLength - 1: currentSlide -1)
+        setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide -1);
     };
-    
-    useEffect(() => {
-        setCurrentSlide(0)
-    }, [])
+
 
     function auto() {
-        
+        slideInterval = setInterval(handleNextSlide, intervalTime);
     }
 
-    useEffect(() => {
-        if (autoScroll) {
-            const auto = () => {
-                slideInterval = setInterval(handleNextSlide, intervalTime)
-            }
-            // auto();
-        }
+    useEffect(()=> {
+        auto();
         return () => clearInterval(slideInterval);
-    },[currentSlide, slideInterval, autoScroll, ])
+    },[currentSlide, autoScroll])
 
-return (
-    <section className={styles.slider}>
-        <div className={`container ${styles.box} group`}>
-            <div className={`${styles.arrow} group-hover:block ${styles.prev}`}>
-                <BsChevronCompactLeft 
-                    onClick={handlePrevSlide} 
-                    size={30}
-                />
-            </div>
-            
-            <div className={`${styles.arrow} group-hover:block ${styles.next}`}>
-                <BsChevronCompactRight
-                    onClick={handleNextSlide} 
-                    size={30}
-                />
-            </div>
-            
-            {sliderData.map((slide, index) => {
-                const { image } = slide;
+    return (
+        <section className={styles.slider}>
+            <div className={`container ${styles.box} group`}>
+                <div className={`${styles.arrow} group-hover:block ${styles.prev}`}>
+                    <BsChevronCompactLeft
+                        onClick={handlePrevSlide}
+                        size={30}
+                    />
+                </div>
+                <div className={`${styles.arrow} group-hover:block ${styles.next}`}>
+                    <BsChevronCompactRight
+                        onClick={handleNextSlide}
+                        size={30}
+                    />
+                </div>
 
-                return (
-                    <div key={index} className={index === currentSlide ? 'slide current' : 'slide'}>
-                        {index === currentSlide && (
+
+                {sliderData.map((slide, index) => {
+                    const { image } = slide;
+
+                    return (
+                        <div key={index} className={index === currentSlide ? 'slide current' : 'slide'}>
+                            {index === currentSlide && (
                                 <img src={image} alt="slide" />
-                        )}
-                    </div>
-                )
-            })}
+                            )}
+                        </div>
+                    )
+                })}
 
-            <div className={styles.dot}>
-                {sliderData.map((slide, index) => (
-                    <div>
-                        <RxDotFilled />
-                    </div>
-                ))}
+                <div className={styles.dot}>
+                    {sliderData.map((slide, index) => (
+                        <div className='text-2xl'>
+                            <RxDotFilled />
+                        </div>
+                    ))}
+                </div>
+
             </div>
-
-        </div>
-    </section>
-)
+        </section>
+    )
 }
 
 export default Slider
