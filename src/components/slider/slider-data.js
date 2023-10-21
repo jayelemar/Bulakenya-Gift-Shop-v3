a9178de521499e3280d6b9e7a6f5a84878433f70
Jay Termulo <jetermulo@gmail.com>
2023-10-21 09:23:03 +0800

    styles: update slider image arrangement

--- src/components/slider/slider-data.js
+++ src/components/slider/slider-data.js
@@ -4,22 +4,15 @@ export const sliderData = [
 
     },
     {
-        image: "https://down-ph.img.susercontent.com/file/ph-11134207-7qul7-lfdkd1mzswtbd3",
+        image: "https://down-ph.img.susercontent.com/file/16e36bdcf33a225dae88a130bf66bbf2",
+    },
+    {
+        image: "https://down-ph.img.susercontent.com/file/ph-11134207-23020-3kgevgqyadnv32",
     },
     {
         image: "https://down-ph.img.susercontent.com/file/02784ef3cbde0fe82da3ed6c0c7ec7d1",
     },
     {
-        image: "https://down-ph.img.susercontent.com/file/ph-11134207-23020-3kgevgqyadnv32",
-    },
-   
-    {
-        image: "https://down-ph.img.susercontent.com/file/16e36bdcf33a225dae88a130bf66bbf2",
-    },
-    {
         image: "https://down-ph.img.susercontent.com/file/ph-11134207-7qul7-lfdkd1mzswtbd3",
     },
-
-
-    
 ];
