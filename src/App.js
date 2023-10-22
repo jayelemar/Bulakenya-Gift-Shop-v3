884f3543746983a476983607979e15b959ab54e8
Jay Termulo <jetermulo@gmail.com>
2023-10-23 03:01:33 +0800

    build: admin page and components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Home, NotFound, Login, Register, Reset } from './pages'
+import { Home, NotFound, Login, Register, Reset, Admin } from './pages'
 import { Header, Footer } from './components'
 import { ToastContainer } from 'react-toastify';
+import AdminOnlyRoute from './components/adminOnlyRoute/AdminOnlyRoute';
 
 function App() {
 
-  return (
+    return (
         <>
-          <Header />
-          <Routes>
-              <Route path="/" element={ <Home /> } />
-              <Route path="*" element={ <NotFound /> } />
-              <Route path="/login" element={ <Login /> } />
-              <Route path="/register" element={ <Register /> } />
-              <Route path="/reset" element={ <Reset /> } />
-          </Routes>
-          <Footer />
-          <ToastContainer position="top-right" autoClose={3000} />
+            <Header />
+            <Routes>
+                <Route path="/" element={ <Home /> } />
+                <Route path="*" element={ <NotFound /> } />
+                <Route path="/login" element={ <Login /> } />
+                <Route path="/register" element={ <Register /> } />
+                <Route path="/reset" element={ <Reset /> } />     
+
+                <Route path="/admin/*" element={
+                    <AdminOnlyRoute>
+                        <Admin /> 
+                    </AdminOnlyRoute>
+                    }
+                />
+                
+            </Routes>
+            <Footer />
+            <ToastContainer position="top-right" autoClose={3000} />
         </>
-  );
+    );
 }
 
 export default App;
